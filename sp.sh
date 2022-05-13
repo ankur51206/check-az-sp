69851fec404ad27379c7dd74fde6d865210c5cf4
ankur51206 <32640688+ankur51206@users.noreply.github.com>
2022-05-14 00:04:54 +0530

    Add files via upload

---
+++ sp.sh
@@ -0,0 +1,18 @@
+#!/bin/bash 
+
+
+az ad app list --query [].{AppId:appId} -o table > appid
+
+sed -i -e '1,2d' appid
+
+
+while IFS= read  -r line;
+do
+
+echo $line >> sp.txt
+az ad app credential list --id $line --query [].{endDate:endDate} -o tsv >> sp.txt
+echo  "--------------------------------------------" >> sp.txt
+echo "" >> sp.txt
+sleep 3
+done < appid
+
